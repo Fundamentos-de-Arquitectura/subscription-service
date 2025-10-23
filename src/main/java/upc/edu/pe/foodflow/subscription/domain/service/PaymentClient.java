e30c75855a7f2f052833bd4db69d880774370fa9
Paolo <u20221f613@upc.edu.pe>
2025-10-23 14:22:04 -0500

    feat(model): add domain service

---
+++ src/main/java/upc/edu/pe/foodflow/subscription/domain/service/PaymentClient.java
@@ -0,0 +1,10 @@
+package upc.edu.pe.foodflow.subscription.domain.service;
+
+
+import upc.edu.pe.foodflow.subscription.application.dto.PaymentData;
+import upc.edu.pe.foodflow.subscription.application.dto.PaymentResult;
+
+public interface PaymentClient {
+
+    PaymentResult processPayment(PaymentData data);
+}
